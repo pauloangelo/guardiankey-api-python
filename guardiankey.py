676708fffacd4e2d119be670ccab41e6ff562dfb
Paulo Angelo <pa@pauloangelo.com>
2018-10-30 20:20:09 -0200

    save

--- guardiankey.py
+++ guardiankey.py
@@ -17,65 +17,73 @@ GKconfig['reverse'] = True
 
 # You need define how the your system get client informations (IP, User-agent):
 def getClientIP():
-	#...
-	ip = "1.1.1.1" # SET-ME
-	return ip
-	
+    #...
+    ip = "1.1.1.1" # SET-ME
+    return ip
+
 def getUserAgent():
-	#...
-	UA="Set-me"
-	return UA
+    #...
+    UA="Set-me"
+    return UA
 
-def create_message(username):
-	global GKconfig
-	keyb64      = GKconfig['key']
-	ivb64 	    = GKconfig['iv']
-	agentid     = GKconfig['agentid']
-	orgid       = GKconfig['orgid']
-	authgroupid = GKconfig['authgroupid']
-	reverse     = GKconfig['reverse']
-	timestamp   = int(time.time())
-	
-	if agentid is not None:
-		key = base64.b64decode(keyb64)
-		iv  = base64.b64decode(ivb64)
-		clientIP = getClientIP()
-		UA = getUserAgent()
-		sjson = {}
-		sjson['generatedTime'] = timestamp
-		sjson['agentId'] = agentid
-		sjson['organizationId'] = orgid
-		sjson['authGroupId'] = authgroupid
-		sjson['service'] = GKconfig['service']
-		sjson['clientIP'] = clientIP
-		sjson['clientReverse'] = socket.gethostbyname_ex(clientIP) if reverse else ""
-		sjson['userName'] = username
-		sjson['authMethod'] = ''
-		sjson['loginFailed'] = '0'
-		sjson['userAgent'] = UA
-		sjson['psychometricTyped'] = ''
-		sjson['psychometricImage'] = ''
-		message = json.dumps(sjson)
-		obj = AES.new(key,AES.MODE_CFB, iv, segment_size=8)
-		cryptmessage = base64.b64encode(obj.encrypt(message))
-		return cryptmessage
-		
-def sendevent(username):
-	global GKconfig
-	message = create_message(username)
-	payload = GKconfig['authgroupid']+"|"+message
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.sendto(payload, ('collector.guardiankey.net', 8888))
+def create_message(username,loginfailed=0,eventType='Authentication'):
+    global GKconfig
+    keyb64      = GKconfig['key']
+    ivb64         = GKconfig['iv']
+    agentid     = GKconfig['agentid']
+    orgid       = GKconfig['orgid']
+    authgroupid = GKconfig['authgroupid']
+    reverse     = GKconfig['reverse']
+    timestamp   = int(time.time())
+    
+    if agentid is not None:
+        key = base64.b64decode(keyb64)
+        iv  = base64.b64decode(ivb64)
+        clientIP = getClientIP()
+        UA = getUserAgent()
+        sjson = {}
+        sjson['generatedTime'] = timestamp
+        sjson['agentId'] = agentid
+        sjson['organizationId'] = orgid
+        sjson['authGroupId'] = authgroupid
+        sjson['service'] = GKconfig['service']
+        sjson['clientIP'] = clientIP
+        try:
+            sjson['clientReverse'] = socket.gethostbyaddr(clientIP)[0] if reverse else ""
+        except:
+            sjson['clientReverse'] = ""
+        sjson['userName'] = username
+        sjson['authMethod'] = ''
+        sjson['loginFailed'] = str(loginfailed)
+        sjson['userAgent'] = UA
+        sjson['psychometricTyped'] = ''
+        sjson['psychometricImage'] = ''
+        sjson['event_type'] = eventType
+        message = json.dumps(sjson)
+        obj = AES.new(key,AES.MODE_CFB, iv, segment_size=8)
+        cryptmessage = base64.b64encode(obj.encrypt(message))
+        return cryptmessage
+        
+def sendevent(username,loginfailed=0,eventType='Authentication'):
+    global GKconfig
+    message = create_message(username,loginfailed,eventType)
+    payload = GKconfig['authgroupid']+"|"+message
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(payload, ('collector.guardiankey.net', 8888))
 
-def checkaccess(username):
-	global GKconfig
-	message = create_message(username)
-	tmpdata = {}
-	tmpdata['id'] = GKconfig['authgroupid']
-	tmpdata['message'] = message
-	data = json.dumps(tmpdata)
-	url = 'https://api.guardiankey.io/checkaccess'
-	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	query = requests.post(url, data=data, headers=headers)
-	return query.text
+def checkaccess(username,loginfailed=0,eventType='Authentication'):
+    global GKconfig
+    message = create_message(username,loginfailed,eventType)
+    tmpdata = {}
+    tmpdata['id'] = GKconfig['authgroupid']
+    tmpdata['message'] = message
+    data = json.dumps(tmpdata)
+    url = 'https://api.guardiankey.io/checkaccess'
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    query = requests.post(url, data=data, headers=headers)
+    try:
+        gkreturn = json.loads(query.text)
+        return gkreturn
+    except:
+        return {"response":"ERROR"}
 
